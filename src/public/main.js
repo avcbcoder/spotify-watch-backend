5d9b0d7dfa3d55ad50e5a4ec1a7bb58237f747a4
Ankit Verma <ankitvermamat@gmail.com>
2020-10-12 02:33:12 +0530

    frontend step 1 done - input user, initiate spotify auth

--- src/public/main.js
+++ src/public/main.js
@@ -1,3 +1,16 @@
+const BASE_URL = `http://localhost:7000`;
+const CLIENT_ID = `c0f2e448149f4e6fbfc38771d9cfb151`;
+
+const spotifyAuthURI = (choosenUsername) =>
+  `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${BASE_URL}/connect-spotify&response_type=code&state=${choosenUsername}`;
+
+const initiateSpotifyAuth = () => {
+  const input = document.getElementById("choosen-username");
+  const choosenUsername = input.value;
+  window.open(spotifyAuthURI(choosenUsername), "_self");
+};
+
 window.onload = () => {
-  console.log("loaded now playing");
+  const spotifyAuthButton = document.getElementById("initiate-auth");
+  spotifyAuthButton.onclick = initiateSpotifyAuth;
 };
